2c273d28b4925d4e0498fb809151be1b889e2005
Dion Vu <dionvu11@gmail.com>
2024-02-21 12:38:12 -0800

    Cleaned up projects file, and added documentation.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,14 @@
 import '../style/style.css';
 import Project from './project';
 import createProjectButton from './projectForm';
-import { displayProject } from './project';
 
-const projectsContainer = document.getElementById('projects-container')
+// const projectsContainer = document.getElementById('projects-container');
 
 export let projectList: Project[] = [];
 
-projectList.push(new Project("Title", "Describing words"));
-
-projectList[0].setProjectElements(
-  displayProject(projectsContainer, projectList[0])
-);
-
-projectList[0].addEditListeners();
-
-logProjects(projectList);
-
-export default function deleteProject(targetProject: Project) {
-  const index = projectList.findIndex((project: Project) => {
-    if (project.title === targetProject.title
-      && project.description === targetProject.description)
-      return true;
-  });
-  projectList.splice(index, index + 1);
-
-  logProjects(projectList);
-};
-
-function logProjects(projectList: Project[]): void {
-  console.table(projectList);
-  console.log('table');
-}
+// function logProjects(projectList: Project[]): void {
+//   console.table(projectList);
+//   console.log('table');
+// }
 
 createProjectButton();
--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,10 @@
-import TodoItem from "./todoItem"
-import deleteProject from ".";
+import { projectList } from ".";
+// import TodoItem from "./todoItem"
 
 export default class Project {
   title: string;
   description: string;
-  todoItemList: TodoItem[];
+  // todoItemList: TodoItem[];
 
   // Container, title, description, remove button
   domElements: HTMLElement[];
@@ -14,33 +14,25 @@ export default class Project {
     this.description = description;
   }
 
-  addTodoItem(item: TodoItem) {
-    this.todoItemList.push(item);
-  }
+  // addTodoItem(item: TodoItem) { this.todoItemList.push(item); };
 
-  getDescription(): string {
-    return this.description;
-  }
-
+  getDescription(): string { return this.description; };
   setDescription(description: string): void {
     this.description = description;
     this.domElements[2].textContent = description;
-  }
+  };
 
-  getTitle(): string {
-    return this.title;
-  }
-
+  getTitle(): string { return this.title; };
   setTitle(title: string): void {
     this.title = title;
     this.domElements[1].textContent = title;
-  }
+  };
 
-  setProjectElements(elements: HTMLElement[]): void {
-    this.domElements = elements;
-  }
+  setProjectElements(elements: HTMLElement[]): void { this.domElements = elements; };
 
-
+  /**
+   * @brief Allows user to edit the project title & description by clicking on them.
+   */
   addEditListeners(): void {
     this.domElements[1].addEventListener('click', () => {
       this.setTitle(prompt("Enter new name."));
@@ -52,6 +44,14 @@ export default class Project {
   }
 }
 
+/**
+ * @brief Displays the project in the dom.
+ *
+ * @param container The project container. 
+ * @param project The project object to be displayed
+ * 
+ * @return Array containing the created dom elements.
+ */
 export function displayProject(container: HTMLElement, project: Project): HTMLElement[] {
   const newProject = document.createElement('div');
   newProject.classList.add('project');
@@ -72,17 +72,32 @@ export function displayProject(container: HTMLElement, project: Project): HTMLEl
   return [newProject, projectTitle, projectDescription, removeButton];
 }
 
+/**
+ * @brief Removes a Project from the main list of projects.
+ * @param projectDom The dom container of the project.
+ * @param targetProject The project to be removed.
+ */
+function removeProject(projectDom: HTMLElement, targetProject: Project): void {
+  const index = projectList.findIndex((project: Project) => {
+    if (project.title === targetProject.title
+      && project.description === targetProject.description)
+      return true;
+  });
+  projectList.splice(index, index + 1);
+  projectDom.remove();
+}
+
+/**
+ * @brief Helper function for displayProject that creates the remove button on each project,
+ * and adds a listener that calls deleteProject.
+ * @param targetProject The project to be removed.
+ * @return The dom elemnt of the button.
+ */
 function createRemovebutton(newProject: HTMLElement, project: Project): HTMLElement {
   const removeButton = document.createElement('button');
   removeButton.textContent = 'Remove project';
   removeButton.addEventListener('click', () => {
     removeProject(newProject, project);
   });
-
   return removeButton;
-}
-
-function removeProject(projectDom: HTMLElement, project: Project) {
-  projectDom.remove();
-  deleteProject(project);
 }
